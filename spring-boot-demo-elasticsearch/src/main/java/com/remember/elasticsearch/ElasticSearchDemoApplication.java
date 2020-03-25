18ae48ab093aa32bb7c1f451cdba6858b9614681
1332661444@qq.com <1332661444@qq.com>
2020-03-25 21:30:33 +0800

    Initial commit

---
+++ spring-boot-demo-elasticsearch/src/main/java/com/remember/elasticsearch/ElasticSearchDemoApplication.java
@@ -0,0 +1,17 @@
+package com.remember.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangjiahao
+ * @date 2020/3/25
+ */
+@SpringBootApplication
+public class ElasticSearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchDemoApplication.class, args);
+	}
+
+}
