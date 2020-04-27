730bfb0d58baed181f80f2ee0e58646b197678c8
wangjiahao <1332661444@qq.com>
2020-04-27 15:05:56 +0800

    1.修改gitignore

---
+++ spring-boot-demo-swagger/src/test/java/com/remember/swagger/SpringBootDemoSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remember.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
