38a899f4abc8ee411ec4695baa185f464dacad07
wangjiahao <1332661444@qq.com>
2020-04-30 14:52:28 +0800

    1.新增mp的代码生成

---
+++ spring-boot-demo-mybatis-plus/src/main/java/com/remember/mybatisplus/entity/QueryRequest.java
@@ -0,0 +1,35 @@
+package com.remember.mybatisplus.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @author wangjiahao
+ */
+@Data
+@ToString
+public class QueryRequest implements Serializable {
+
+    private static final long serialVersionUID = -4869594085374385813L;
+    /**
+     * 当前页面数据量
+     */
+    private int pageSize = 10;
+    /**
+     * 当前页码
+     */
+    private int pageNum = 1;
+    /**
+     * 排序字段
+     */
+    private String field;
+    /**
+     * 排序规则，asc升序，desc降序
+     */
+    private String order;
+
+
+
+}
---
+++ spring-boot-demo-mybatis-plus/src/main/java/com/remember/mybatisplus/generator/CodeGenerator.java
@@ -0,0 +1,121 @@
+package com.remember.mybatisplus.generator;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * mybatis plus 提供的代码生成器
+ * 可以快速生成 Entity、Mapper、Mapper XML、Service、Controller 等各个模块的代码
+ * https://mp.baomidou.com/guide/generator.html
+ *
+ * @author wangjiahao
+ * @date 2019/08/22
+ */
+public class CodeGenerator {
+
+    // 项目名称，maven父子项目使用
+    private static final String PROJECT_NAME = "spring-boot-demo-mybatis-plus";
+    // 数据库 URL
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/spring_boot_demo?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
+    // 数据库驱动
+    private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    // 数据库用户名
+    private static final String USERNAME = "root";
+    // 数据库密码
+    private static final String PASSWORD = "123456";
+    // @author 值
+    private static final String AUTHOR = "wangjiahao";
+    // 包的基础路径
+    private static final String BASE_PACKAGE_URL = "com.remember.mybatisplus";
+    // xml文件路径
+    private static final String XML_PACKAGE_URL = "/src/main/resources/mapper/";
+    // xml 文件模板
+    private static final String XML_MAPPER_TEMPLATE_PATH = "generator/templates/mapper.xml";
+    // mapper 文件模板
+    private static final String MAPPER_TEMPLATE_PATH = "generator/templates/mapper.java";
+    // entity 文件模板
+    private static final String ENTITY_TEMPLATE_PATH = "generator/templates/entity.java";
+    // service 文件模板
+    private static final String SERVICE_TEMPLATE_PATH = "generator/templates/service.java";
+    // serviceImpl 文件模板
+    private static final String SERVICE_IMPL_TEMPLATE_PATH = "generator/templates/serviceImpl.java";
+    // controller 文件模板
+    private static final String CONTROLLER_TEMPLATE_PATH = "generator/templates/controller.java";
+
+    public static void main(String[] args) {
+        AutoGenerator generator = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig globalConfig = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        if(StringUtils.isBlank(PROJECT_NAME)){
+            globalConfig.setOutputDir(projectPath  +"/src/main/java");
+        }else{
+            globalConfig.setOutputDir(projectPath + File.separator + PROJECT_NAME +"/src/main/java");
+        }
+
+        globalConfig.setAuthor(AUTHOR);
+        globalConfig.setOpen(false);
+        globalConfig.setFileOverride(false);
+        generator.setGlobalConfig(globalConfig);
+
+        // 数据源配置
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setUrl(URL);
+        dataSourceConfig.setDriverName(DRIVER_NAME);
+        dataSourceConfig.setUsername(USERNAME);
+        dataSourceConfig.setPassword(PASSWORD);
+        generator.setDataSource(dataSourceConfig);
+
+        // 包配置
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setModuleName("test");
+        packageConfig.setParent(BASE_PACKAGE_URL);
+        generator.setPackageInfo(packageConfig);
+
+        // 配置自定义代码模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setXml(XML_MAPPER_TEMPLATE_PATH);
+        templateConfig.setMapper(MAPPER_TEMPLATE_PATH);
+        templateConfig.setEntity(ENTITY_TEMPLATE_PATH);
+        templateConfig.setService(SERVICE_TEMPLATE_PATH);
+        templateConfig.setServiceImpl(SERVICE_IMPL_TEMPLATE_PATH);
+        templateConfig.setController(CONTROLLER_TEMPLATE_PATH);
+        generator.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        strategy.setInclude(scanner("表名"));
+//        strategy.setInclude("customer_complain");
+        strategy.setSuperEntityColumns("id");
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(packageConfig.getModuleName() + "_");
+//        strategy.setTablePrefix(packageConfig.getModuleName());
+        generator.setStrategy(strategy);
+        generator.setTemplateEngine(new FreemarkerTemplateEngine());
+        generator.execute();
+    }
+
+    private static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(("请输入" + tip + "："));
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotBlank(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+}
