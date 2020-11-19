0b15ce53bdb7e144d07cb9ef7d1a22eae957d303
Jiahao <1332661444@qq.com>
2020-11-19 21:53:58 +0800

    新增admin模块

---
+++ spring-boot-demo-admin/admin-client1/src/main/java/com/remember/admin/client1/AdminClient1Application.java
@@ -0,0 +1,13 @@
+package com.remember.admin.client1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClient1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClient1Application.class, args);
+    }
+
+}
---
+++ spring-boot-demo-admin/admin-client2/src/main/java/com/remember/admin/client2/AdminClient2Application.java
@@ -0,0 +1,13 @@
+package com.remember.admin.client2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClient2Application.class, args);
+    }
+
+}
