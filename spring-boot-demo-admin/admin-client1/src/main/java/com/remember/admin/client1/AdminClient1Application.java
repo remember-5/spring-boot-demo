4335095499a6c7dcc39c580d7d10345d54f9a97d
wangjiahao <1332661444@qq.com>
2020-11-20 10:36:49 +0800

    删除不必要代码

--- spring-boot-demo-admin/admin-client1/src/main/java/com/remember/admin/client1/AdminClient1Application.java
+++ spring-boot-demo-admin/admin-client1/src/main/java/com/remember/admin/client1/AdminClient1Application.java
@@ -3,6 +3,9 @@ package com.remember.admin.client1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangjiahao
+ */
 @SpringBootApplication
 public class AdminClient1Application {
 
