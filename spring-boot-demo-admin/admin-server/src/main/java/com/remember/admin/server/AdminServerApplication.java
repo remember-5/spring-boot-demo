19ebf18b93ece1c5a5c42594e026cf7f7e8f72d7
wangjiahao <1332661444@qq.com>
2020-11-20 10:07:28 +0800

    增加admin模块

--- spring-boot-demo-admin/admin-server/src/main/java/com/remember/admin/server/AdminServerApplication.java
+++ spring-boot-demo-admin/admin-server/src/main/java/com/remember/admin/server/AdminServerApplication.java
@@ -1,8 +1,13 @@
 package com.remember.admin.server;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangjiahao
+ */
+@EnableAdminServer
 @SpringBootApplication
 public class AdminServerApplication {
 
