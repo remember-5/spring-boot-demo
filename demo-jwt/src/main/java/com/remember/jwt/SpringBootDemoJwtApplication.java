09861c2fb5627700177dde4af83f8d3e943df7a0
Jiahao <1332661444@qq.com>
2021-09-26 23:06:53 +0800

    修改名字错误

--- demo-jwt/src/main/java/com/remember/jwt/SpringBootDemoJwtApplication.java
+++ demo-jwt/src/main/java/com/remember/jwt/SpringBootDemoJwtApplication.java
@@ -3,6 +3,9 @@ package com.remember.jwt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangjiahao
+ */
 @SpringBootApplication
 public class SpringBootDemoJwtApplication {
 
