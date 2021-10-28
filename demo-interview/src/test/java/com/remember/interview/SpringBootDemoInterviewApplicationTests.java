edbbfe235a267dd240cc9c3b6fb7302fa6fa306a
wangjiahao <1332661444@qq.com>
2021-10-28 19:54:33 +0800

    tiktok获取

--- demo-interview/src/test/java/com/remember/interview/SpringBootDemoInterviewApplicationTests.java
+++ demo-interview/src/test/java/com/remember/interview/SpringBootDemoInterviewApplicationTests.java
@@ -8,6 +8,7 @@ class SpringBootDemoInterviewApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
---
+++ demo-web/src/main/java/com/remember/demo/web/TikTok.java
@@ -0,0 +1,12 @@
+package com.remember.demo.web;
+
+import lombok.Data;
+
+/**
+ * @author wangjiahao
+ * @date 2021/10/28
+ */
+@Data
+public class TikTok {
+    private String shareContent;
+}
