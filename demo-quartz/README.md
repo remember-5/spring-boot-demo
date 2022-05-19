139bb5b9266da883e4a24297ed98043314eb1a5a
wangjiahao <1332661444@qq.com>
2022-05-19 19:37:21 +0800

    增加quartz集群版本的定时任务

---
+++ demo-quartz/README.md
@@ -0,0 +1,3 @@
+使用quartz集群
+
+表结构解释https://www.jianshu.com/p/b94ebb8780fa
---
+++ demo-quartz/src/main/java/com/remember/quartz/DemoQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.remember.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoQuartzApplication.class, args);
+	}
+
+}
