0b64e3b127eba1c02a0f4da5c1434af7832f0cb4
wangjiahao <1332661444@qq.com>
2022-06-28 22:59:05 +0800

    增加ws的鉴权

---
+++ demo-websocket/netty/src/main/java/com/remember/netty/websocket/SocketMessage.java
@@ -0,0 +1,34 @@
+package com.remember.netty.websocket;
+
+import lombok.Data;
+
+import java.util.Set;
+
+/**
+ * @author fly
+ * @description
+ * @date 2021/4/15 15:19
+ */
+@Data
+public class SocketMessage {
+    /**
+     * 消息内容
+     */
+    private String msg;
+    /**
+     * 发送目标
+     */
+    private Set<String> users;
+
+    public SocketMessage() {
+    }
+
+    public SocketMessage(String msg, Set<String> users) {
+        this.msg = msg;
+        this.users = users;
+    }
+
+    public SocketMessage(String msg) {
+        this.msg = msg;
+    }
+}
