1880f373315962fd131e1637be22bbad49ce5c02
wangjiahao <1332661444@qq.com>
2023-03-17 14:27:50 +0800

    修改groupid

---
+++ demo-liquibase/src/test/java/com/remember5/liquibase/LiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remember5.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
