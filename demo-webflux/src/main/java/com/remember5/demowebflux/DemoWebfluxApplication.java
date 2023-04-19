32959136b782b5ba96d2224b0f4170f1ece20ff0
wangjiahao <1332661444@qq.com>
2023-04-19 17:54:41 +0800

    增加webflux

---
+++ demo-webflux/src/main/java/com/remember5/demowebflux/DemoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.remember5.demowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebfluxApplication.class, args);
+    }
+
+}
