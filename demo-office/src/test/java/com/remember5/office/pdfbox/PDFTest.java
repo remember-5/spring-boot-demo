0c61a633b70c9f5ac1feb2e42593aa9a4ad84b03
wangjiahao <1332661444@qq.com>
2023-06-14 13:39:55 +0800

    删除多余的test代码

--- demo-office/src/test/java/com/remember5/office/pdfbox/PDFTest.java
+++ demo-office/src/test/java/com/remember5/office/pdfbox/PDFTest.java
@@ -15,8 +15,13 @@
  */
 package com.remember5.office.pdfbox;
 
+import com.remember5.office.utils.PdfBoxUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author wangjiahao
@@ -28,5 +33,29 @@ public class PDFTest {
     @Test
     public void splice() {
 
+    }
+
+    @Test
+    void pdfJointImg() throws IOException {
+
+        String originalPdfUrl = "https://jf.sh.189.cn/minio/gov-miniapp/testpdf.pdf";
+        String jointImageUrl = "https://jf.sh.189.cn/minio/gov-miniapp/testimg.png";
+        String savePath = "/Users/wangjiahao/Downloads/";
+        String resultPdfPath = savePath + "result.pdf";
+        int x = 100;
+        int y = 140;
+        int width = 63;
+        int height = 90;
+        int pageNum = 10;
+
+
+        // 网络资源下载到路径
+//        File originalPdfFile = HttpDownloader.downloadForFile(originalPdfUrl, new File(savePath), -1, null);
+//        File jointImageFile = HttpDownloader.downloadForFile(jointImageUrl, new File(savePath), -1, null);
+//        PdfBoxUtil.imgInPdf(originalPdfFile, jointImageFile, resultPdfPath, pageNum, x, y, width, height);
+//        final String fileName = ResourceFileUtil.getAbsolutePath("static/font/simfang.ttf");
+        ClassPathResource classPathResource = new ClassPathResource("static/fonts/simfang.ttf");
+        File fontFile = classPathResource.getFile();
+        PdfBoxUtils.textInPdf(savePath + "pdf1.pdf", "中国电信上海分公司", savePath + "result1.pdf", 0, 180, 660, fontFile, 12);
     }
 }
--- demo-office/src/test/java/com/remember5/office/poi/PoiTest.java
+++ demo-office/src/test/java/com/remember5/office/poi/PoiTest.java
@@ -31,9 +31,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
 
 import static com.remember5.office.utils.WordUtils.doWriter;
 
@@ -60,5 +59,106 @@ public class PoiTest {
         FileInputStream fileInputStream =  new FileInputStream(ResourceFileUtil.getFile("files/test.docx"));
         WordUtils.doWriter(fileInputStream, dataMap, resultSavePath);
     }
+
+//    @Test
+//    void test1() throws Exception {
+//        // 模板注意 用{} 来表示你要用的变量 如果本来就有"{","}" 特殊字符 用"\{","\}"代替
+//        String templateFileName = "/Users/wangjiahao/Downloads/simple.xlsx";
+//
+//        // 方案1 根据对象填充
+//        String fileName = "/Users/wangjiahao/Downloads/simpleFill" + System.currentTimeMillis() + ".xlsx";
+//        // 这里 会填充到第一个sheet， 然后文件流会自动关闭
+//        FillData fillData = new FillData();
+//        fillData.setName("张三");
+//        fillData.setNumber(5.2);
+//        EasyExcel.write(fileName).withTemplate(templateFileName).sheet().doFill(fillData);
+//
+//        //加载Excel文档
+////        Workbook wb = new Workbook();
+////        wb.loadFromFile(fileName);
+////        //调用方法保存为PDF格式
+////        wb.saveToFile("/Users/wangjiahao/Downloads/a.pdf", FileFormat.PDF);
+//
+//
+//        // Create Workbook to load Excel file
+////        Workbook workbook = new Workbook(fileName);
+//
+//        // Create PDF options
+////        PdfSaveOptions options = new PdfSaveOptions();
+////        options.setCompliance(PdfCompliance.PDF_A_1_B);
+////        options.setOnePagePerSheet(true);
+////        // Save the document in PDF format
+////        workbook.save("/Users/wangjiahao/Downloads/Excel-to-PDF.pdf", options);
+//
+//
+////        // 方案2 根据Map填充
+////        fileName = "/Users/wangjiahao/Downloads/simpleFill" + System.currentTimeMillis() + ".xlsx";
+////        // 这里 会填充到第一个sheet， 然后文件流会自动关闭
+////        Map<String, Object> map = new HashMap<>();
+////        map.put("name", "张三");
+////        map.put("number", 5.2);
+////        EasyExcel.write(fileName).withTemplate(templateFileName).sheet().doFill(map);
+//    }
+//
+//
+//    @Test
+//    void testExcel2Json() {
+//        String filePath = "/Users/wangjiahao/Downloads/64e90931-751c-4017-83d8-bc97f9a0f8a1.xlsx";
+//        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
+//        List<Map> excelList = EasyExcel.read(filePath).sheet().doReadSync();
+//
+//        final Object[] keys = excelList.get(0).values().toArray();
+//        excelList.remove(0);
+//
+//        ArrayList<JSON> jsons = new ArrayList<>();
+//        excelList.forEach(list -> {
+//            final Object[] originalRowData = JSON.parseObject(list.toString()).values().toArray();
+//            final JSONObject newRowData = new JSONObject();
+//
+//            for (int i = 0; i < keys.length; i++) {
+//                newRowData.put(keys[i].toString(), originalRowData[i]);
+//            }
+//            jsons.add(newRowData);
+//        });
+//        System.err.println(jsons);
+//    }
+//
+//
+//    @Test
+//    void testExcel2Content() throws InterruptedException {
+//        String filePath = "/Users/wangjiahao/Downloads/test.xlsx";
+//
+//        ExcelReader reader = ExcelUtil.getReader(filePath);
+//        // sheet = 0
+//        List<List<Object>> readAll = reader.read();
+//
+//        final String sheet0Str = list2String(readAll);
+//        // 获取多页
+//        final int sheetCount = reader.getSheetCount();
+//        if (sheetCount > 1) {
+//            StringBuilder result = new StringBuilder(sheet0Str);
+//            for (int i = 1; i < sheetCount; i++) {
+//                reader.setSheet(i);
+//                final List<List<Object>> read = reader.read();
+//                result.append(list2String(read));
+//            }
+//            return;
+//        }
+//        new CountDownLatch(0).await();
+//
+//    }
+//
+//
+//    private String list2String(List<List<Object>> data) {
+//        StringBuilder sb = new StringBuilder();
+//        for (List<Object> datum : data) {
+//            for (Object obj : datum) {
+//                if (null != obj) {
+//                    sb.append(obj);
+//                }
+//            }
+//        }
+//        return sb.toString();
+//    }
 
 }
